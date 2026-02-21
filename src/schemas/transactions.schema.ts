234403984639ba2f07534cfa8d89f05ada6965f0
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-02-21 22:15:47 +0530

    feat(schema): add schema to validate user transaction history query result

--- src/schemas/transactions.schema.ts
+++ src/schemas/transactions.schema.ts
@@ -1,9 +1,12 @@
 import z from "zod";
 import {
   AssetTypeEnum,
+  ledgerTypeEnum,
   TransactionStatusEnum,
   TransactionTypeEnum,
+  WalletTypeEnum,
 } from "./enums.js";
+import { LedgerInDBSchema } from "./ledger.schema.js";
 
 // Transaction metadata in DB.
 export const TransactionMetadataSchema = z.object({
@@ -29,10 +32,52 @@ export const TransactionInDBSchema = z.object({
 });
 export type TransactionInDB = z.infer<typeof TransactionInDBSchema>;
 
+// Select ID only from transactions table.
 export const TransactionIdSelectSchema = TransactionInDBSchema.pick({
   id: true,
 });
 export type TransactionIdSelect = z.infer<typeof TransactionIdSelectSchema>;
+
+// The schema for return type of SQL query that retrieves
+// User's transaction history with ledger details.
+export const GetUserHistorySchema = z.array(
+  TransactionInDBSchema.omit({
+    user_id: true,
+    idempotency_key: true,
+    id: true,
+    type: true,
+  })
+    .extend({
+      transaction_id: TransactionInDBSchema.shape.id,
+      transaction_type: TransactionInDBSchema.shape.type,
+      ledgerEntries: z.array(
+        z.object({
+          ledger_id: LedgerInDBSchema.shape.id,
+          wallet_type: WalletTypeEnum,
+          asset_type: AssetTypeEnum,
+          entry_type: ledgerTypeEnum,
+          amount: LedgerInDBSchema.shape.amount,
+          description: LedgerInDBSchema.shape.description,
+        }),
+      ),
+    })
+    .transform((data) => ({
+      transactionId: data.transaction_id,
+      transactionType: data.transaction_type,
+      status: data.status,
+      metadata: data.metadata,
+      createdAt: data.created_at,
+      ledgerEntries: data.ledgerEntries.map((entry) => ({
+        ledgerId: entry.ledger_id,
+        walletType: entry.wallet_type,
+        assetType: entry.asset_type,
+        entryType: entry.entry_type,
+        amount: entry.amount,
+        description: entry.description,
+      })),
+    })),
+);
+export type GetUserHistory = z.infer<typeof GetUserHistorySchema>;
 
 // Request Body
 export const TransactionRequestBodySchema = z.object({
