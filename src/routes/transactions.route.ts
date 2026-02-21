2d08805c980500df8b099ce2df35bf02211c2ffd
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-02-21 14:39:22 +0530

    feat(route): setup /transactions route for executing transactions

---
+++ src/routes/transactions.route.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import assignClientDetails from "../middlewares/assignClientDetails.js";
+import validateRequest from "../middlewares/validateRequest.js";
+import { TransactionRequestBodySchema } from "../schemas/transactions.schema.js";
+import { createTransaction } from "../controllers/transactions.controller.js";
+
+const transactionRouter = Router();
+
+transactionRouter.post(
+  "/transactions",
+  assignClientDetails,
+  validateRequest({ bodySchema: TransactionRequestBodySchema }),
+  createTransaction,
+);
+
+export default transactionRouter;
