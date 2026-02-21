14253f7c26c8a2dd4466bcdfde02205f3acf6068
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-02-21 15:22:21 +0530

    feat(schema): add register user request schema

--- src/schemas/users.schema.ts
+++ src/schemas/users.schema.ts
@@ -8,3 +8,15 @@ export const UserInDBSchema = z.object({
   created_at: z.coerce.date(),
 });
 export type UserInDB = z.infer<typeof UserInDBSchema>;
+
+// Request Body schema for registering new user.
+export const RegisterUserRequestBodySchema = z.object({
+  username: z
+    .string()
+    .min(1, { error: "Username is required." })
+    .max(50, "Username must be less than 50 characters long."),
+  email: z.email({ error: "Invalid email." }),
+});
+export type RegisterUserRequestBody = z.infer<
+  typeof RegisterUserRequestBodySchema
+>;
