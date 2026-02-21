029c6eca99683313987e7f3c15992f5c65ee0f98
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-02-21 12:58:56 +0530

    feat(schema): infer and export types for all enums

--- src/schemas/enums.ts
+++ src/schemas/enums.ts
@@ -7,15 +7,20 @@ export const TransactionTypeEnum = z.enum([
   "SPEND",
   "PENALTY",
 ]);
+export type TransactionType = z.infer<typeof TransactionTypeEnum>;
 
 export const AssetTypeEnum = z.enum(["CP", "CREDITS"]);
+export type AssetType = z.infer<typeof AssetTypeEnum>;
 
 export const TransactionStatusEnum = z.enum(["SUCCESS", "FAILED", "PENDING"]);
+export type TransactionStatus = z.infer<typeof TransactionStatusEnum>;
 
 export const WalletTypeEnum = z.enum([
   "USER",
   "SYSTEM_REVENUE",
   "SYSTEM_TREASURY",
 ]);
+export type WalletType = z.infer<typeof WalletTypeEnum>;
 
 export const ledgerTypeEnum = z.enum(["DEBIT", "CREDIT"]);
+export type LedgerType = z.infer<typeof ledgerTypeEnum>;
